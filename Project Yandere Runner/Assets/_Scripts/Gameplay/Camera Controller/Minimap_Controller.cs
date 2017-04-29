046ebeb25851fcc4e7f626629d8b50fb0f42f7f9
Rachel Vancleave <sindiewen@gmail.com>
2017-04-29 10:18:30 -0700

    Started implementing the minimap

---
+++ Project Yandere Runner/Assets/_Scripts/Gameplay/Camera Controller/Minimap_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap_Controller : MonoBehaviour
+{
+	// public variables
+	//	Players
+	[HeaderAttribute("Players")]
+	public Transform Ichiro;		// Reference to the Ichiro Player
+	public Transform Yumi;			// Reference to the Yumi Player
+	
+	// Private Variables
+	private Camera camComponent;		// SStores reference to the camera component
+	
+	
+	
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		// Initializes the Camera Component
+		camComponent = GetComponent<Camera>();	
+		
+		camComponent.orthographicSize = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
